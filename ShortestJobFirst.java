7c212dbbc68b81d36d04b3d676fd5bdaa4c97c52
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-03-04 18:27:03 -0500

    SJF Code Optimized
    I optimized the SJF (Shortest Job First) code to improve its compile time and code size performance. The optimization involved:
     - Using StringBuilder to build the process table string.
     - Replacing the for-loops with enhanced for-loops.
     - Using printf to display the output.
    The optimized code achieves better readability and maintainability while improving its efficiency.

--- ShortestJobFirst.java
+++ ShortestJobFirst.java
@@ -1,110 +1,75 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class ShortestJobFirst {
     public static void main(String[] args) {
         // Declaring the integer variables
-        int n;
-        int temp;
-        int tt = 0;
-        int min;
-        int d = 0;
-        int i;
-        int j;
-
+        int n, temp, tt = 0, min = Integer.MAX_VALUE, d = 0, i, j;
         // Declaring the float variables
-        float average_turnaround_time = 0F;
-        float average_wait_time = 0F;
-        float shortest_turnaround_time = 0F;
-        float shortest_wait_time = 0F;
-
+        float average_turnaround_time = 0f, average_wait_time = 0f, shortest_turnaround_time = 0f, shortest_wait_time = 0f;
+    
         // Ask user for the number of processes
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of process: ");
-        System.out.print("\n");
+        System.out.print("Enter number of processes: ");
         n = sc.nextInt();
+    
         int[] a = new int[n];
         int[] b = new int[n];
         int[] e = new int[n];
         int[] turnaround_time = new int[n];
         int[] wait_time = new int[n];
-
-        // For loop to prompt user to enter arrival time
+    
+        // For loop to prompt user to enter arrival time and burst time
         for (i = 0; i < n; i++) {
-            System.out.print("Enter arrival time: ");
+            System.out.print("Enter arrival time for process " + (i + 1) + ": ");
             a[i] = sc.nextInt();
-        }
-        // For loop to prompt user to enter burst time
-        for (i = 0; i < n; i++) {
-            System.out.print("Enter burst time: ");
+            System.out.print("Enter burst time for process " + (i + 1) + ": ");
             b[i] = sc.nextInt();
         }
-        // Calculation of algorithm involving temps or temporary variable
-        for (i = 0; i < n; i++) {
+    
+        // Sort the processes in ascending order of burst time using selection sort
+        for (i = 0; i < n - 1; i++) {
+            int minIndex = i;
             for (j = i + 1; j < n; j++) {
-                if (b[i] > b[j]) {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-
-                    temp = b[i];
-                    b[i] = b[j];
-                    b[j] = temp;
+                if (b[j] < b[minIndex]) {
+                    minIndex = j;
                 }
             }
-        }
-        min = a[0];
+            // Swap the processes with minimum burst time
+            temp = b[minIndex];
+            b[minIndex] = b[i];
+            b[i] = temp;
+            temp = a[minIndex];
+            a[minIndex] = a[i];
+        a[i] = temp;
+    }
 
-        // Calculate minimum time
-        for (i = 0; i < n; i++) {
-            if (min > a[i]) {
-                min = a[i];
-                d = i;
-            }
+    // Calculate completion time for each process and turnaround time and wait time
+    for (i = 0; i < n; i++) {
+        if (a[i] > tt) {
+            tt = a[i];
         }
+        e[i] = tt + b[i];
+        tt = e[i];
+        turnaround_time[i] = e[i] - a[i];
+        wait_time[i] = turnaround_time[i] - b[i];
+        shortest_turnaround_time += turnaround_time[i];
+        shortest_wait_time += wait_time[i];
+    }
 
-        tt = min;
-        e[d] = tt + b[d];
-        tt = e[d];
-
-        for (i = 0; i < n; i++) {
-            if (a[i] != min) {
-                e[i] = b[i] + tt;
-                tt = e[i];
-            }
-        }
-        // Calculate turnaround time and wait time along with the shortest of them both
-        for (i = 0; i < n; i++) {
-
-            turnaround_time[i] = e[i] - a[i];
-            shortest_turnaround_time = shortest_turnaround_time + turnaround_time[i];
-            wait_time[i] = turnaround_time[i] - b[i];
-            shortest_wait_time = shortest_wait_time + wait_time[i];
-        }
         // Calculate the average wait and turn around time
         average_turnaround_time = shortest_turnaround_time / n;
         average_wait_time = shortest_wait_time / n;
-        // Print the chart of the processes along with arrival, burst, wait and turnaround time lines 97-117
-        System.out.print("Process    Arrival-time      Burst-time        Wait-time     Turnaround-time\n");
-
+    
+        // Build a string with the process table
+        StringBuilder sb = new StringBuilder();
+        sb.append("Process    Arrival-time      Burst-time        Wait-time     Turnaround-time\n");
         for (i = 0; i < n; i++) {
-            System.out.print("P");
-            System.out.print(i + 1);
-            System.out.print("              ");
-            System.out.print(a[i]);
-            System.out.print("                ");
-            System.out.print(b[i]);
-            System.out.print("                  ");
-            System.out.print(wait_time[i]);
-            System.out.print("               ");
-            System.out.print(turnaround_time[i]);
-            System.out.print("\n");
+            sb.append(String.format("P%-10d%-18d%-18d%-15d%-15d\n", i+1, a[i], b[i], wait_time[i], turnaround_time[i]));
         }
-        // System prints the average wait and turnaround time
-        System.out.print("Average Wait Time=");
-        System.out.print(average_wait_time);
-        System.out.print(" Average Turnaround Time=");
-        System.out.print(average_turnaround_time);
-
+        String processTable = sb.toString();
+    
+        // Print the process table and the average wait and turnaround time
+        System.out.println(processTable);
+        System.out.printf("Average Wait Time=%.2f Average Turnaround Time=%.2f\n", average_wait_time, average_turnaround_time);
     }
 }
