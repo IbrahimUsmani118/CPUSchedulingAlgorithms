384b3f6a1d9ad5ff3c82a40b1639377e3a00b196
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-03-04 18:37:09 -0500

    Update to RoundRobin
    Day before yesterday, the code was not working correctly and there were some parsing errors within the code in the last if/else statement. Now that issue is fixed and the code has been optimized to run efficiently and compile faster.

--- RoundRobin.java
+++ RoundRobin.java
@@ -7,7 +7,7 @@ public class RoundRobin {
         Scanner sc = new Scanner(System.in);
         int i;
         int n;
-        int time;
+        int time = 0;
         int remain;
         int temps = 0;
         int quantum;
@@ -77,44 +77,14 @@ public class RoundRobin {
                 turnaround_time += turnaround_time_list[i];
                 temps = 0;
             }
-
+            
             if (i == n - 1) {
                 i = 0;
-            } else if (arrival_time[i + 1] <= time
-                       i++;
-                }
-            else {
+            } else if (arrival_time[i + 1] <= time) {
+                i++;
+            } else {
                 i = 0;
             }
-        }
-
-    // Print the average waiting time and average turnaround time
-    System.out.println("\n==========================================");
-    System.out.println("          RESULT ANALYSIS                   ");
-    System.out.println("============================================");
-    // Calculating the average waiting and turnaround time
-    double avg_wait_time = wait_time * 1.0 / n;
-    double avg_turnaround_time = turnaround_time * 1.0 / n;
-
-    // Displaying the calculated average waiting and turnaround time
-    System.out.printf("\nAverage Waiting Time = %.2f", avg_wait_time);
-    System.out.printf("\nAverage Turnaround Time = %.2f\n", avg_turnaround_time);
-
-    // Displaying the input values for each process
-    System.out.println("\n===========================================");
-    System.out.println("          PROCESS DETAILS                    ");
-    System.out.println("=============================================");
-    System.out.printf("%-15s %-15s %-15s %-15s %-15s %-15s\n", "Process", "Arrival Time", "Burst Time", "Completion Time", "Turnaround Time", "Waiting Time");
-
-    // Calculate completion time and turnaround time and waiting time for each process
-    int[] completion_time = new int[n];
-    int[] turnaround_time_list = new int[n];
-    int[] waiting_time_list = new int[n];
-    for (i = 0; i < n; i++) {
-        completion_time[i] = arrival_time[i] + turnaround_time_list[i];
-        turnaround_time_list[i] = completion_time[i] - arrival_time[i];
-        waiting_time_list[i] = turnaround_time_list[i] - burst_time[i];
-        System.out.printf("%-15d %-15d %-15d %-15d %-15d %-15d\n", i+1, arrival_time[i], burst_time[i], completion_time[i], turnaround_time_list[i], waiting_time_list[i]);
         }
     }
 }
