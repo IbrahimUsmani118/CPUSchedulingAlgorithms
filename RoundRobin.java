36d93afcf66f3cc26e59c11a6135d2e212f2812b
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-11-30 08:38:54 -0500

    Update RoundRobin.java
    One implementation calculates Round Robin times (wait, turnaround) and produces the Gantt Chart, while the other solely generates the Gantt Chart without computing scheduling parameters.

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,90 +1,64 @@
 import java.util.Scanner;
 
 public class RoundRobin {
+    public static final String ANSI_BOLD = "\u001B[1m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        // Initializing variables for the program to perform its tasks
         Scanner sc = new Scanner(System.in);
         int i;
         int n;
         int time = 0;
-        int remain;
-        int temps = 0;
         int quantum;
 
-        // Initialize wait and turnaround time to 0 as the initial starting point
-        int wait_time = 0;
-        int turnaround_time = 0;
-
-        // Ask user for the total number of processes to calculate its times
-        System.out.println("=============================================");
+        System.out.println(ANSI_BOLD + "=============================================");
         System.out.println("              ROUND ROBIN SCHEDULING          ");
-        System.out.println("=============================================");
-        System.out.print("Enter the total number of processes: ");
+        System.out.println("=============================================" + ANSI_RESET);
+        System.out.print(ANSI_BOLD + "Enter the total number of processes: " + ANSI_RESET);
         n = sc.nextInt();
-        remain = n;
 
-        // Arrival, burst and return time
         int[] arrival_time = new int[n];
         int[] burst_time = new int[n];
         int[] remaining_burst_time = new int[n];
-        int[] completion_time = new int[n];
-        int[] turnaround_time_list = new int[n];
-        int[] waiting_time_list = new int[n];
 
-        // User is prompted to enter the arrival and burst time for the # processes desired
-        System.out.println("\nEnter the arrival time and burst time for each process: ");
+        System.out.println("\n" + ANSI_BOLD + "Enter the arrival time and burst time for each process: " + ANSI_RESET);
         for (i = 0; i < n; i++) {
-            System.out.print("\nProcess " + (i + 1) + ":\n");
-            System.out.print("Arrival time: ");
+            System.out.print("\n" + ANSI_BOLD + "Process " + (i + 1) + ":" + ANSI_RESET);
+            System.out.print("\n" + ANSI_BOLD + "Arrival time: " + ANSI_RESET);
             arrival_time[i] = sc.nextInt();
-            System.out.print("Burst time: ");
+            System.out.print(ANSI_BOLD + "Burst time: " + ANSI_RESET);
             burst_time[i] = sc.nextInt();
             remaining_burst_time[i] = burst_time[i];
         }
 
-        // Enter the quantum value for the scheduling task
-        System.out.println("\n=============================================");
-        System.out.print("Enter the quantum value: ");
+        System.out.println("\n" + ANSI_BOLD + "=============================================");
+        System.out.print("Enter the quantum value: " + ANSI_RESET);
         quantum = sc.nextInt();
 
-        System.out.println("\n\n=========================================");
-        System.out.println("                   GANTT CHART               ");
-        System.out.println("=============================================");
+        // Store the Gantt Chart information in a StringBuilder
+        StringBuilder ganttChart = new StringBuilder();
 
-        System.out.print("0");
-        for (time = 0, i = 0; remain != 0;) {
-            if (remaining_burst_time[i] <= quantum && remaining_burst_time[i] > 0) {
-                time += remaining_burst_time[i];
-                System.out.print("  |  P" + (i+1) + "  |  " + time);
-                remaining_burst_time[i] = 0;
-                temps = 1;
-            } else if (remaining_burst_time[i] > 0) {
-                remaining_burst_time[i] -= quantum;
-                time += quantum;
-                System.out.print("  |  P" + (i+1) + "  |  " + time);
-            }
-
-            if (remaining_burst_time[i] == 0 && temps == 1) {
-                remain--;
-                completion_time[i] = time;
-                turnaround_time_list[i] = completion_time[i] - arrival_time[i];
-                waiting_time_list[i] = turnaround_time_list[i] - burst_time[i];
-                // Displaying the result of waiting, turn around time:
-                System.out.printf("\n\nP[%d]\t:\tTurnaround Time = %d\tWaiting Time = %d",
-                    i + 1, turnaround_time_list[i], waiting_time_list[i]);
-                wait_time += waiting_time_list[i];
-                turnaround_time += turnaround_time_list[i];
-                temps = 0;
-            }
-            
-            if (i == n - 1) {
-                i = 0;
-            } else if (arrival_time[i + 1] <= time) {
-                i++;
-            } else {
-                i = 0;
+        // Generating the Gantt Chart
+        for (i = 0; i < n; i++) {
+            ganttChart.append("P").append(i + 1).append("  |  ").append(arrival_time[i]);
+            if (i != n - 1) {
+                ganttChart.append("  |  ");
             }
         }
+
+        // Print the Gantt Chart after all processes have been executed
+        System.out.println("\n\n" + ANSI_BOLD + "=========================================");
+        System.out.println("                   GANTT CHART               ");
+        System.out.println("=============================================" + ANSI_RESET);
+
+        System.out.println("");
+
+        System.out.print(ganttChart.toString());
+
+        System.out.println("");
+        System.out.println("");
+
+        // Close the Scanner object after using it
+        sc.close();
     }
 }
