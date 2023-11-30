c2d6571dfb18ab7685fd984d9217c1a46633face
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-11-30 08:37:22 -0500

    Update FirstComeFirstServe.java
    Added color-coded output.

--- FirstComeFirstServe.java
+++ FirstComeFirstServe.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class FirstComeFirstServe {
+    // ANSI color codes for matrix-like output and bold text
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BOLD = "\u001B[1m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         int n; // n is the variable representing the number of processes in the algorithm
         /* the burst time, wait time, and turnaround time have an index of 10 indicating
@@ -15,12 +20,12 @@ public class FirstComeFirstServe {
         int i;
         int j;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter total number of processes: ");
+        System.out.print(ANSI_BOLD + "Enter total number of processes: " + ANSI_RESET);
         n = sc.nextInt();
 
-        System.out.print("\nEnter Process Burst Time: \n");
+        System.out.print("\n" + ANSI_BOLD + "Enter Process Burst Time: \n" + ANSI_RESET);
         for (i = 0; i < n; i++) {
-            System.out.print("P[" + (i + 1) + "]: ");
+            System.out.print(ANSI_BOLD + "P[" + (i + 1) + "]: " + ANSI_RESET);
             burst_time[i] = sc.nextInt();
         }
 
@@ -34,20 +39,20 @@ public class FirstComeFirstServe {
             }
         }
 
-        System.out.println("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+        System.out.println("\n" + ANSI_BOLD + ANSI_GREEN + "Process\t\tBurst Time\tWaiting Time\tTurnaround Time" + ANSI_RESET);
 
         // Calculating turnaround time
         for (i = 0; i < n; i++) {
             turnaround_time[i] = burst_time[i] + wait_time[i];
             average_wait_time += wait_time[i];
             average_turnaround_time += turnaround_time[i];
-            System.out.println("P[" + (i + 1) + "]\t\t" + burst_time[i] + "\t\t" + wait_time[i] + "\t\t" + turnaround_time[i]);
+            System.out.println(ANSI_GREEN + "P[" + (i + 1) + "]\t\t" + burst_time[i] + "\t\t" + wait_time[i] + "\t\t" + turnaround_time[i] + ANSI_RESET);
         }
 
         // Printing average waiting and turnaround times
         average_wait_time /= i;
         average_turnaround_time /= i;
-        System.out.println("\nAverage Waiting Time:\t" + average_wait_time);
-        System.out.println("Average Turnaround Time:\t" + average_turnaround_time);
+        System.out.println("\n" + ANSI_BOLD + ANSI_GREEN + "Average Waiting Time:\t" + average_wait_time + ANSI_RESET);
+        System.out.println(ANSI_BOLD + ANSI_GREEN + "Average Turnaround Time:\t" + average_turnaround_time + ANSI_RESET);
     }
 }
